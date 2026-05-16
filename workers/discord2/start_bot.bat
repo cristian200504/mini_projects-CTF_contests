a3067a4e2ea643231f79dbc24cc400750a5d7194
cristian200504 <100048895+cristian200504@users.noreply.github.com>
2026-05-16 17:53:50 +0300

    delete tokens
    resetted but still deteled them

--- workers/discord2/start_bot.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo ========================================================
-echo Starting LLM Discord Army Bot 2...
-echo ========================================================
-set DISCORD_TOKEN=MTQ5MTQzOTkzNjY4NjEyOTMyNA.GphJbk.dmekzteEd6cNAovagJ95qxbHIN7aWguoqr4jIY
-cd %~dp0
-python discord_bot.py
-pause
